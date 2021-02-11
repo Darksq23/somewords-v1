398b15a78f8c5add49f2bd344611be5c1d81ff28
berking-da-quan <ishimweeschylebersy@gmail.com>
2021-02-11 21:59:24 +0200

    updated the search logic

--- search.php
+++ search.php
@@ -11,8 +11,8 @@ if(isset($_GET['s']) && !empty($_GET['s'])) {
 }
 
 //$data = $dbconnection->query('select * from lyrics');
-$song = $dbconnection->query('select * from lyrics where song_name like "%'.$q.'%"');
-$artist = $dbconnection->query('select * from lyrics where artist like "%'.$q.'%"');
+$song = $dbconnection->query('select * from lyrics where song_name like "%'.$q.'%" ORDER BY id DESC');
+$artist = $dbconnection->query('select * from lyrics where artist like "%'.$q.'%" ORDER BY id DESC');
 ?>
 
     <div class="search__box">
@@ -26,14 +26,19 @@ $artist = $dbconnection->query('select * from lyrics where artist like "%'.$q.'%
             </button>
         </form>
     </div>
-
-<h3>Result for "<?= $q ?>"</h3>
-<?php while ($res = $song->fetch()): ?>
-    <p><?= $res['song_name'] ?></p>
-<?php endwhile ?>
-<?php while ($res = $artist->fetch()): ?>
-    <p><?= $res['song_name'] ?></p>
-<?php endwhile ?>
+    <div class="container box">
+        <?php if($song->rowCount() <= 0 && $artist->rowCount() <= 0): ?>
+            <h3>Sorry but nothing found! :(</h3>
+        <?php else: ?>
+            <h3>Result for "<?= $q ?>"</h3>
+            <?php while ($res = $song->fetch()): ?>
+                <p><?= $res['song_name'] ?></p>
+            <?php endwhile ?>
+            <?php while ($res = $artist->fetch()): ?>
+                <p><?= $res['song_name'] ?></p>
+            <?php endwhile ?>
+        <?php endif ?>
+    </div>
 
 <?php 
 require "footer.php";
